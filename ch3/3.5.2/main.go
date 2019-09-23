82d48428f75458348af16b586d5c090a567c9859
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-09-23 23:32:43 +0900

    chapter 3 (until 3.5.2)

---
+++ ch3/3.5.2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+func main() {
+	data := []byte{0x0, 0x0, 0x27, 0x10}
+	var i int32
+	binary.Read(bytes.NewReader(data), binary.BigEndian, &i)
+	// binary.Read(bytes.NewReader(data), binary.LittleEndian, &i)
+	fmt.Printf("data: %d\n", i)
+}
